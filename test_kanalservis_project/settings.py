548aee00be14e8c8b8cc8b5769b3c30d24f1542b
krasovskiyhst <79247009+krasovskiyhst@users.noreply.github.com>
2022-05-25 02:19:03 +0700

    frontend

--- test_kanalservis_project/settings.py
+++ test_kanalservis_project/settings.py
@@ -54,6 +54,9 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 )
+if not DEBUG:
+    # IP адрес сервера из переменной вирт. окружения
+    CORS_ORIGIN_WHITELIST = tuple(os.getenv('ALLOWED_HOSTS').split(' ')[-1] + ':3000')
 
 ROOT_URLCONF = 'test_kanalservis_project.urls'
 
